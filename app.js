c1aad1dba3c0dda36a1f3c44582592fc1b29edbd
Oshanlakshith <oshanlakshitha146@gmail.com>
2022-09-03 10:30:25 +0530

    update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mongoose=require('mongoose')
 const app = express()
 const customer=require('./routes/customer')
 const item=require('./routes/item')
@@ -8,13 +7,6 @@ const port = 4000
 
 app.use(express.json())
 
-const url='mongodb://localhost/express'
-mongoose.connect(url,{useNewUrlParser:true})
-const con=mongoose.connection
-
-con.on("open",()=>{
-  console.log("Mongodb Connected");
-})
 app.use('/customer',customer)
 app.use('/item',item)
 app.use('/order',order)
